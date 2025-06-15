f04b59c67c5ccb8fa3c63b045c4f2201995543ea
Motunrayo Adeneye <67984447+motuncoded@users.noreply.github.com>
2025-06-15 23:05:20 +0100

    gtask done

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -11,3 +11,17 @@ export interface ButtonProps {
   onClick: () => void;
   type?: "button" | "submit" | "reset";
 }
+
+
+export interface PropertyProps{
+  name: string;
+  rating: string;
+  address: {
+    city: string;
+    country: string;
+  };
+  image: string;
+  description: string;
+  category: string[];
+  amenity:any
+}
